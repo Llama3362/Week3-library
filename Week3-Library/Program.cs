fccf6ce6257563a2e49bc9d547dde0708e00549a
B00984998 <smith-d44@ulster.ac.uk>
2025-10-06 14:59:04 +0100

    Added code to initalise object: Book

--- Week3-Library/Program.cs
+++ Week3-Library/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    static void Main(string[] args)
+    {
+        //Create a new instance of Book class
+        Book book = new Book();
+
+        //First new instance of book class
+        book.Title = "C# for beginners";
+        book.Author = "BillGates";
+        book.ISBN = "12345678";
+
+        //Second new instance of Book class
+        Book book1 = new Book();
+        book1.Title = "C# Methods and Classes";
+        book1.Author = "Microsoft";
+        book1.ISBN = "55667778";
+
+        // Output the first books information to the console
+        Console.WriteLine($"Book title: {book.Title}");
+        Console.WriteLine($"Book author: {book.Author}");
+        Console.WriteLine($"Book ISBN: {book.ISBN}");
+
+        // Output the second books information to the console
+        Console.WriteLine($"Book title: {book1.Title}");
+        Console.WriteLine($"Book author: {book1.Author}");  
+        Console.WriteLine($"Book ISBN: {book1.ISBN}");
+    }
+}
